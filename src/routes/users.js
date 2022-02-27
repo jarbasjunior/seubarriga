58d177ed1e8e76877f930a489cbd4111ec91f403
jarbasjunior <jarbasoujunior@gmail.com>
2022-02-27 15:10:36 -0300

    fix_feat: search and user creation through postgres

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,12 @@
-module.exports = () => {
+module.exports = (app) => {
   const findAll = (req, res) => {
-    const user = [
-      { name: 'John Doe', mail: 'john_doe@email.com' },
-    ];
-    res.status(200).json(user);
+    app.db('users').select()
+      .then((result) => res.status(200).json(result));
   };
 
-  const create = (req, res) => {
-    res.status(201).json(req.body);
+  const create = async (req, res) => {
+    const result = await app.db('users').insert(req.body, '*');
+    res.status(201).json(result[0]);
   };
 
   return { findAll, create };
