7c9d99940db790c1e3cffddc38dfdd109cae902c
jarbasjunior <jarbasoujunior@gmail.com>
2022-02-27 10:36:16 -0300

    feat: added /users route

--- src/app.js
+++ src/app.js
@@ -4,4 +4,11 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
+app.get('/users', (req, res) => {
+  const user = [
+    { name: 'John Doe', mail: 'john_doe@email.com' },
+  ];
+  res.status(200).json(user);
+});
+
 module.exports = app;
