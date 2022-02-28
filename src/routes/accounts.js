1ce6d1086c096eee0a4dd725d365242d99b4b94e
jarbasjunior <jarbasoujunior@gmail.com>
2022-02-28 11:02:58 -0300

    feat: added account listing

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,7 +1,12 @@
 module.exports = (app) => {
+  const findAll = (req, res) => {
+    app.services.account.findAll()
+      .then((result) => res.status(200).json(result));
+  };
+
   const create = async (req, res) => {
-    const result = await app.services.accounts.save(req.body);
+    const result = await app.services.account.save(req.body);
     return res.status(201).json(result[0]);
   };
-  return { create };
+  return { findAll, create };
 };
