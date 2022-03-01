a5f2922c09c47689cf2879cf32ec554db302f62a
jarbasjunior <jarbasoujunior@gmail.com>
2022-03-01 15:21:57 -0300

    test: should not authenticate with non-existent e-mail

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -14,11 +14,21 @@ test('Deve receber token ao se autenticar', () => {
     });
 });
 
-test('Não deve autenticar com senha inválido', () => {
+test('Não deve autenticar com senha inválida', () => {
   const user = { name: 'Walter', mail: `${Date.now()}@email.com`, password: '123456' };
   return app.services.user.save(user)
     .then(() => request(app).post('/auth/signin')
       .send({ mail: user.mail, password: '654321' }))
+    .then((res) => {
+      expect(res.status).toBe(401);
+      expect(res.body.token).not.toBeNull();
+      expect(res.body.error).toBe('Acesso negado! Usuário e/ou senha inválidos.');
+    });
+});
+
+test('Não deve autenticar com e-mail inválido', () => {
+  return request(app).post('/auth/signin')
+    .send({ mail: 'nao_existo_na_base@email.com', password: '654321' })
     .then((res) => {
       expect(res.status).toBe(401);
       expect(res.body.token).not.toBeNull();
