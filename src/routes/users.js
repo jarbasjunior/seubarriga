10ef2cf561dda6cd5b3b81e36c2c3f2600881925
jarbasjunior <jarbasoujunior@gmail.com>
2022-03-01 16:12:36 -0300

    refact: save user

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,13 +5,10 @@ module.exports = (app) => {
       .catch((err) => next(err));
   };
 
-  const create = async (req, res, next) => {
-    try {
-      const result = await app.services.user.save(req.body);
-      return res.status(201).json(result[0]);
-    } catch (err) {
-      return next(err);
-    }
+  const create = (req, res, next) => {
+    app.services.user.save(req.body)
+      .then((result) => res.status(201).json(result[0]))
+      .catch((err) => next(err));
   };
 
   return { findAll, create };
