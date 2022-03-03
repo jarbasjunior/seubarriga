4190a8ae1a066d1a95023fe8afeab91348c6697d
jarbasjunior <jarbasoujunior@gmail.com>
2022-03-03 11:48:01 -0300

    feat: deny another user's resource access

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -18,7 +18,10 @@ module.exports = (app) => {
 
   router.get('/:id', (req, res, next) => {
     app.services.account.read({ id: req.params.id })
-      .then((result) => res.status(200).json(result))
+      .then((result) => {
+        if (result.user_id !== req.user.id) return res.status(403).json({ error: 'Recurso não está disponível para este usuário!' });
+        return res.status(200).json(result);
+      })
       .catch((err) => next(err));
   });
 
