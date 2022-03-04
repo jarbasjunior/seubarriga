7a33b9c132c511bba1a0ff6d1acf59bdab63ae85
jarbasjunior <jarbasoujunior@gmail.com>
2022-03-04 10:24:05 -0300

    feat: validate input and output values

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -12,7 +12,14 @@ module.exports = (app) => {
   };
 
   const create = (transaction) => {
-    return app.db('transactions').insert(transaction, '*');
+    const newTransaction = { ...transaction };
+
+    if ((transaction.type === 'I' && transaction.ammount < 0)
+      || (transaction.type === 'O' && transaction.ammount > 0)) {
+      newTransaction.ammount *= -1;
+    }
+
+    return app.db('transactions').insert(newTransaction, '*');
   };
 
   const update = (id, transaction) => {
