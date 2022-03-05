dacfaebfafbc3f87c303f44649aa3868cbc7d832
jarbasjunior <jarbasoujunior@gmail.com>
2022-03-05 16:20:36 -0300

    refact_test: insert transfer successfully

--- test/routes/transfers.test.js
+++ test/routes/transfers.test.js
@@ -21,29 +21,50 @@ test('Deve listar apenas as transferências do usuário', () => {
     });
 });
 
-test('Deve inserir uma transferência com sucesso', () => {
+describe('Quando inserir uma transferência válida deve:', async () => {
   const date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay(), '00', '00', '00', '0');
   const transfer = { account_origin_id: 10000, account_destiny_id: 10001, description: 'Regular Transfer', user_id: 10000, date, ammount: 50.00 };
-  return request(app).post(MAIN_ROUTE)
-    .set('authorization', `bearer ${TOKEN}`)
-    .send(transfer)
-    .then(async (result) => {
-      expect(result.status).toBe(201);
-      transfer.date = transfer.date.toISOString();
-      transfer.ammount = Math.round((transfer.ammount * 100) / 100).toFixed(2);
-      expect(result.body).toMatchObject(transfer);
+  let transferId;
+  let credit;
+  let debit;
+  let transactions;
 
-      const transactions = await app.db('transactions').where({ transfer_id: result.body.id });
-      const ammountAccOrigin = Math.round(((transfer.ammount * -1) * 100) / 100).toFixed(2);
+  test('Retornar o status 201 e os dados da transferência', () => {
+    return request(app).post(MAIN_ROUTE)
+      .set('authorization', `bearer ${TOKEN}`)
+      .send(transfer)
+      .then(async (result) => {
+        expect(result.status).toBe(201);
+        transfer.date = transfer.date.toISOString();
+        transfer.ammount = Math.round((transfer.ammount * 100) / 100).toFixed(2);
+        expect(result.body).toMatchObject(transfer);
+        transferId = result.body.id;
+      });
+  });
 
-      expect(transactions).toHaveLength(2);
-      expect(transactions[0].description).toBe(`Transfer to account: ${transfer.account_destiny_id}`);
-      expect(transactions[1].description).toBe(`Transfer from account: ${transfer.account_origin_id}`);
-      expect(transactions[0].ammount).toBe(ammountAccOrigin);
-      expect(transactions[1].ammount).toBe(transfer.ammount);
-      expect(transactions[0].type).toBe('O');
-      expect(transactions[1].type).toBe('I');
-      expect(transactions[0].account_id).toBe(transfer.account_origin_id);
-      expect(transactions[1].account_id).toBe(transfer.account_destiny_id);
-    });
+  test('Gerar as transações de entrada e saída para a transferência', async () => {
+    transactions = await app.db('transactions').where({ transfer_id: transferId }).orderBy('ammount');
+    [debit, credit] = transactions;
+    expect(transactions).toHaveLength(2);
+    expect(transactions[0].description).toBe(`Transfer to account: ${transfer.account_destiny_id}`);
+    expect(transactions[1].description).toBe(`Transfer from account: ${transfer.account_origin_id}`);
+  });
+
+  test('A transação de saída deve ser negativa', () => {
+    const ammountAccOrigin = Math.round(((transfer.ammount * -1) * 100) / 100).toFixed(2);
+    expect(debit.ammount).toBe(ammountAccOrigin);
+    expect(debit.type).toBe('O');
+    expect(debit.account_id).toBe(transfer.account_origin_id);
+  });
+
+  test('A transação de entrada deve ser positiva', () => {
+    expect(credit.ammount).toBe(transfer.ammount);
+    expect(credit.type).toBe('I');
+    expect(credit.account_id).toBe(transfer.account_destiny_id);
+  });
+
+  test('As duas transações geradas devem referenciar a transferência que a originou', () => {
+    expect(debit.transfer_id).toBe(transferId);
+    expect(credit.transfer_id).toBe(transferId);
+  });
 });
